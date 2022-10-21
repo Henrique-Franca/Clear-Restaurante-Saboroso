c1df5ebb4aa465410cf50745aa5ba5bde943625a
HenriqueFranca <henriqsf@gmail.com>
2022-10-21 12:34:25 -0300

    Efetuando Reserva com Método POST para o MySQL

--- saboroso/routes/index.js
+++ saboroso/routes/index.js
@@ -1,6 +1,7 @@
 var conn = require('./../inc/db')
 var express = require('express');
 var menus = require('./../inc/menus');
+var reservations = require('./../inc/reservations');
 var router = express.Router();
 
 /* GET home page. */
@@ -50,11 +51,48 @@ router.get('/menus', function(req, res, next){
 //rota para a pagina das reservas
 router.get('/reservations', function(req, res, next){
 
-  res.render('reservations',{
-    title: 'Reservas - Restaurante Saboroso!',
-    background:'images/img_bg_2.jpg',
-    h1:'Reserve uma Mesa!'
-  });
+  reservations.render(req, res);
+
+});
+
+//rota para a pagina das reservas
+router.post('/reservations', function(req, res, next){
+
+  if(!req.body.name){
+    reservations.render(req, res, "digite um nome");
+  }else if(!req.body.email){
+
+    reservations.render(req, res, "digite um e-mail");
+
+  }else if(!req.body.people){
+
+    reservations.render(req, res, "Selecione a quantidade de pessoas");
+
+  }else if(!req.body.date){
+
+    reservations.render(req, res, "Escolha uma data");
+
+  }else if(!req.body.time){
+
+    reservations.render(req, res, "Digite o horário");
+
+  }else{
+
+    reservations.save(req.body).then(results =>{
+
+      req.body = {};
+
+      reservations.render(req, res, null, "Reserva realizada com sucesso!");
+
+    }).catch(err=>{
+
+      reservations.render(req, res, err.message);
+
+    });
+
+  }
+
+  
 
 });
 
