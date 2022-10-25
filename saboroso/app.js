7c7fad881b7d7a07281ea4dbc824e69d91017965
HenriqueFranca <henriqsf@gmail.com>
2022-10-25 12:32:13 -0300

    Administração do Menu - Alterando dados do Menu

--- saboroso/app.js
+++ saboroso/app.js
@@ -5,12 +5,41 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 var RedisStore =  require('connect-redis')(session);
+var formidable = require('formidable');
+var path = require('path');
 
 var indexRouter = require('./routes/index');
 var adminRouter = require('./routes/admin');
 
 
 var app = express();
+
+app.use(function(req,res,next){
+  let contentType = req.headers["content-type"];
+ 
+  if (req.method === 'POST' && contentType.indexOf('multipart/form-data;') > -1){
+
+    var form = new formidable.IncomingForm({
+      uploadDir: path.join(__dirname, "/public/images"),
+      keepExtentions: true
+    });
+
+    form.parse(req, function(err, fields, files){
+
+      req.fields = fields;
+      req.files = files;
+
+      next();
+
+    });
+
+  }else{
+
+    next();
+
+  }
+
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
